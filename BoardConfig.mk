5534ae87dc7e4d7adee48acd0c7c86fd312e112d
Bruno Martins <bgcngm@gmail.com>
2017-12-18 11:20:17 +0000

    wt88047: Use XML audio policy configuration file
     * Switch to audio_policy_configuration.xml instead of using
       the legacy audio_policy.conf.
    
     * Removed multi-channel profile as it is redundant with respect
       to direct output profiles [1].
    
    [1] https://source.codeaurora.org/quic/la/platform/hardware/qcom/audio/commit/?h=LA.UM.6.5.r1-04300-8x96.0&id=ac3415870d63b18694e49d982e89ccb10dbcb2c3
    
    Change-Id: Ia93e30428ff9e27ae409e685a84fa5a8e406bca0

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,6 +22,9 @@ include device/cyanogen/msm8916-common/BoardConfigCommon.mk
 
 DEVICE_PATH := device/wingtech/wt88047
 
+# Audio
+USE_XML_AUDIO_POLICY_CONF := 1
+
 # Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth
 
